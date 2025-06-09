4e55a0d26859e23316e982c596ed8146eba3677c
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-09 16:08:01 -0600

    feat: agregar botón de retroceso y mejorar etiquetas en el formulario de recuperación de contraseña

--- src/components/recuperarContraseña/Formulario.tsx
+++ src/components/recuperarContraseña/Formulario.tsx
@@ -1,10 +1,11 @@
 import { useCambioContraseña } from "../../hooks/CambiarContraseña/CambiarContraseña";
 import { toast } from "sonner";
 import IconoCorrecto from "../../assets/iconos/iconoCorrecto";
+import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
 
 export default function Formulario() {
 
-    const { envio, handleSubmit, handleInput, handleKeyDown, setInputRefs } = useCambioContraseña();
+    const { envio, handleSubmit, handleInput, handleKeyDown, setInputRefs, handleBack } = useCambioContraseña();
 
     const handleSubmitEnvio = (e: React.FormEvent<HTMLFormElement>) => {
         handleSubmit(e);
@@ -16,22 +17,36 @@ export default function Formulario() {
 
     return (
         !envio ? (
-            <form className="flex flex-col items-center justify-center gap-6 " onSubmit={handleSubmitEnvio} >
+            <form className="flex flex-col items-center mt-15 md:max-w-2/6 w-full mx-auto   justify-center gap-6" onSubmit={handleSubmitEnvio} >
+                <div className="absolute top-4 left-4">
+                    <button className=" text-white rounded-md p-2 w-full font-bold text-xl" onClick={() => window.location.href = "/"}>
+                        <ArrowBackOutlinedIcon style={{ color: "#48e", fontWeight: "bold" }} />
+                    </button>
+                </div>
+
                 <h1 className="text-3xl font-semibold max-w-3/4 text-center">Recuperar contraseña</h1>
 
-                <label htmlFor="id_usuario" className="relative">
-                    <input type="text" name="id_usuario" id="id_usuario" placeholder=" " className="border border-input rounded-md px-4 py-4 w-100 input_hover text-xl" />
-                    <span className=" font-semibold absolute top-4 left-5 text-input px-3 bg-white text-xl">Usuario</span>
+                <label htmlFor="id_usuario" className="relative w-full">
+                    <input type="text" name="id_usuario" id="id_usuario" placeholder=" " className="border border-input rounded-md px-4 py-4 w-full input_hover text-xl" />
+                    <span className=" font-semibold absolute top-4 left-5 text-input px-3 bg-white text-xl">Correo Institucional</span>
                 </label>
 
-                <button type="submit" className="bg-primary text-white rounded-md p-2 w-100 font-bold text-xl">Enviar</button>
+                <button type="submit" className="bg-primary text-white rounded-md p-2 w-full font-bold text-xl">Enviar</button>
             </form >
         ) : (
-            <form className="flex flex-col items-center justify-center gap-6" onSubmit={handleSubmit} >
-                <h1 className="text-3xl font-semibold max-w-3/4 text-center">Codigo de verificación</h1>
-                <p className="max-w-2/6 text-center ">Ingrese el código de verificación que te hemos enviado a tu correo institucional</p>
+            <form className="flex flex-col items-center justify-center gap-6 mt-15" onSubmit={handleSubmit} >
 
-                <div id="otp-container" className="flex gap-6 justify-center items-center max-w-2/5">
+                <div className="absolute top-4 left-4">
+                    <button className=" text-white rounded-md p-2 w-full font-bold text-xl" onClick={handleBack}>
+                        <ArrowBackOutlinedIcon style={{ color: "#48e", fontWeight: "bold" }} />
+                    </button>
+                </div>
+
+
+                <h1 className="text-3xl font-semibold md:max-w-3/4 text-wrap text-center">Codigo de verificación</h1>
+                <p className="md:max-w-2/6 text-center ">Ingrese el código de verificación que te hemos enviado a tu correo institucional</p>
+
+                <div id="otp-container" className="flex md:gap-6 gap-2 justify-center items-center md:max-w-2/5 w-full">
                     {Array.from({ length: 6 }).map((_, index) => (
                         <input
                             key={index}
@@ -44,12 +59,12 @@ export default function Formulario() {
                             onChange={(e) => handleInput(e, index)}
                             onKeyDown={(e) => handleKeyDown(e, index)}
                             ref={(el) => setInputRefs(el, index)}
-                            className="border border-input rounded-md px-4 py-4 w-16 text-center text-xl focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
+                            className="border border-input rounded-md px-4 py-4 md:w-16 w-12 text-center text-xl focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
                         />
                     ))}
                 </div>
 
-                <button type="submit" className="bg-primary text-white rounded-md p-2 w-100 font-bold text-xl">Enviar</button>
+                <button type="submit" className="bg-primary text-white rounded-md p-2 w-full font-bold text-xl">Enviar</button>
             </form >
         )
     )
