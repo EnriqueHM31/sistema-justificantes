966e0be1032aee079fc739c4de0c6d07201d1c85
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-09 13:40:53 -0600

    feat: agregar hook useFoto para gestionar la carga y vista previa de imágenes de perfil

---
+++ src/hooks/perfil/Foto.ts
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+
+
+
+export function useFoto() {
+    const [preview, setPreview] = useState<string | null>(null);
+    const [file, setFile] = useState<File | null>(null);
+    const user = useUser();
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = e.target.files?.[0];
+        if (selectedFile) {
+            const imageURL = URL.createObjectURL(selectedFile);
+            setPreview(imageURL);
+            setFile(selectedFile);
+        }
+    };
+
+    const handleAntes = () => {
+        setPreview(null);
+        setFile(null);
+    };
+
+    type tipo = typeof user.user;
+
+    const handleSubmit = async (
+        file: File | null,
+        user: tipo,
+        onSuccessToast: () => void,
+        onErrorToast: (msg?: string) => void
+    ) => {
+        if (!file || !user) {
+            onErrorToast("No hay imagen para subir.");
+            return;
+        }
+
+        try {
+            await user.setProfileImage({ file });
+            onSuccessToast();
+        } catch (error) {
+            console.error("Error al subir la foto:", error);
+            onErrorToast("Hubo un error al actualizar la foto");
+        }
+    };
+
+    return {
+        preview,
+        file,
+        handleImageChange,
+        handleAntes,
+        handleSubmit,
+    };
+}
