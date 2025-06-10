21caeef15cbeb8799bc0b42b5c8cde365020cd44
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-10 15:39:47 -0600

    feat: agregar componente InputFileUpload para carga de archivos CSV

---
+++ src/pages/Administrador/components/FileCSV.tsx
@@ -0,0 +1,44 @@
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+    backgroundColor: '#0b347e',
+});
+
+export default function InputFileUpload() {
+    return (
+        <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<CloudUploadIcon />}
+            sx={{
+                backgroundColor: '#0b347e',
+                color: '#fff',
+                '&:hover': {
+                    backgroundColor: '#0b347e',
+                    color: '#fff',
+                },
+            }}
+        >
+            Carga CSV
+            <VisuallyHiddenInput
+                type="file"
+                onChange={(event) => console.log(event.target.files)}
+                multiple
+                accept='text/csv'
+            />
+        </Button>
+    );
+}
