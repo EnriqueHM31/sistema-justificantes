8c5efd7d2637584fa232e020fb6d8473d74fdec4
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-09 22:03:48 -0600

    fix: ajustar diseño y mejorar la accesibilidad en el componente CambiarFoto

--- src/components/perfil/CambiarFoto.tsx
+++ src/components/perfil/CambiarFoto.tsx
@@ -5,7 +5,7 @@ import { useUser } from "@clerk/clerk-react";
 import { toast } from "sonner";
 
 export default function CambiarFoto() {
-    const { preview, file, handleImageChange, handleAntes, handleSubmit } = useFoto();
+    const { preview, file, handleImageChange, handleSubmit } = useFoto();
     const { user } = useUser();
 
     const handleClick = () => {
@@ -24,23 +24,18 @@ export default function CambiarFoto() {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center w-full h-full gap-4 relative">
-            <h2 className="font-extrabold text-3xl">Cambiar foto</h2>
+        <div className="flex flex-col items-center justify-center w-full h-full gap-4 relative my-10">
+            <h2 className="font-extrabold text-lg md:text-3xl">Cambiar foto</h2>
 
             <div className="flex items-center justify-center w-full h-full">
                 <label
                     htmlFor="dropzone-file"
-                    className="flex flex-col items-center justify-center w-full h-11/12 border-2 border-dashed rounded-lg cursor-pointer"
+                    className="flex flex-col items-center justify-center w-full 
+                    md:h-11/12 md:min-h-[50dvh] h-full border-2 border-dashed rounded-lg cursor-pointer"
                 >
                     <div className="flex flex-col items-center justify-center pt-5 pb-6 h-full">
                         {preview ? (
                             <>
-                                <button
-                                    className="absolute top-2 left-2 bg-gray-200 px-3 py-1 rounded"
-                                    onClick={handleAntes}
-                                >
-                                    Anterior
-                                </button>
                                 <img
                                     src={preview}
                                     alt="Vista previa"
@@ -64,10 +59,10 @@ export default function CambiarFoto() {
                                         d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                                     />
                                 </svg>
-                                <p className="mb-2 text-sm text-gray-700">
-                                    <span className="font-semibold">Haz clic para subir</span> o arrastra
+                                <p className="mb-2 text-xs xl:text-sm text-gray-700 font-semibold">
+                                    <span className="">Haz clic para subir</span> o arrastra
                                 </p>
-                                <p className="text-xs text-gray-500">SVG, PNG, JPG o GIF (MAX. 800x400px)</p>
+                                <p className="text-xs xl:text-sm md:max-w-full max-w-3/4 text-gray-500 text-center">PNG, JPG o JPGE (MAX. 800x400px)</p>
                             </>
                         )}
                     </div>
@@ -85,7 +80,7 @@ export default function CambiarFoto() {
             <button
                 onClick={handleClick}
                 disabled={!file}
-                className="bg-secondary text-white rounded-md px-4 py-2 w-fit max-w-md font-bold text-xl text-start disabled:opacity-50"
+                className="bg-secondary text-white rounded-md px-4 py-2 w-fit max-w-md font-bold text-md xl:text-xl text-start disabled:opacity-50"
             >
                 Subir foto
             </button>
