895f63a0545f2c8f0d1b65f800de2fe7914ad389
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-10 15:39:17 -0600

    feat: agregar interfaz formDataRegistroAdminProps para el registro de administradores

---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+export interface formDataRegistroAdminProps {
+    clave_usuario: string;
+    nombre_usuario: string;
+    apellidos_usuario: string;
+    cargo_usuario: string;
+    correo_usuario: string;
+    carrera: string;
+    file: File | null | string
+}
