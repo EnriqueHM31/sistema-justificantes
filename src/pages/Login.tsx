2ac89c6a36c45719a2d1e954722218fc9f34146f
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-09 19:41:59 -0600

    fix: corregir la extensión de la imagen de fondo en la página de inicio de sesión

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,15 @@
 import Formulario from "../components/login/Formulario";
-import FondoLogin from '../assets/fondos/fondo_login.webp';
+import FondoLogin from '../assets/fondos/fondo_login.png';
 
 export default function Login() {
     return (
         <>
             <div className="fixed inset-0 flex items-center justify-center m-0 p-0">
-                <div className="w-full  flex items-center justify-center ">
+                <div className="w-full  flex items-center justify-center z-50 ">
                     <Formulario />
                 </div>
             </div>
-            <img src={FondoLogin} alt="fondo login" className="w-full h-full absolute inset-0 -z-50" />
+            <img src={FondoLogin} alt="fondo login" className=" absolute -z-50 inset-0 w-full h-full   md:h-screen brightness-100 " />
         </>
     )
 }
