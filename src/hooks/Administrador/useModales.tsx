11eb53f5a426b1372fa32eef499c209e6215beb0
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-10 22:47:56 -0600

    feat: agregar hook useModales para gestionar la visualización de modales en el registro de usuarios

---
+++ src/hooks/Administrador/useModales.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { toast } from "sonner";
+import { type UsuarioJefe } from "../../types";
+
+export function useModales() {
+
+    const [mostrarModalRegistrar, setMostrarModalRegistrar] = useState({
+        registrar: false,
+        confirmar_registro: false,
+        documento_csv: false,
+        confirmar_documento_csv: false
+    });
+
+    const handleModalConfirmacionRegistro = ({ formData }: { formData: UsuarioJefe }) => {
+
+        if (!formData.clave_usuario || !formData.nombre_usuario || !formData.apellidos_usuario || !formData.correo_usuario || !formData.carrera) {
+            return toast.error("Los campos no pueden estar vacíos")
+        }
+
+        setMostrarModalRegistrar(prev => ({ ...prev, registrar: true }));
+    }
+
+
+    const handleModalCancelarRegistro = () => {
+        setMostrarModalRegistrar(prev => ({ ...prev, registrar: false }));
+    }
+
+    const handleModalConfirmacionDocumento = () => {
+        setMostrarModalRegistrar(prev => ({ ...prev, documento_csv: true }));
+    }
+
+    const handleModalCancelarDocumento = () => {
+        setMostrarModalRegistrar(prev => ({ ...prev, documento_csv: false }));
+    }
+
+    const handleBDDocumentoCSV = () => {
+        setMostrarModalRegistrar(prev => ({ ...prev, documento_csv: false }));
+    };
+
+
+    return {
+        mostrarModalRegistrar,
+        handleModalConfirmacionRegistro,
+        handleModalCancelarRegistro,
+        handleModalConfirmacionDocumento,
+        handleModalCancelarDocumento,
+        handleBDDocumentoCSV,
+    }
+}
