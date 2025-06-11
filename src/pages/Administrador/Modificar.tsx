a386c703937f2a67302736ff18be01d1bffa541a
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-10 22:48:10 -0600

    feat: agregar funcionalidad de búsqueda y paginación en el componente Modificar

--- src/pages/Administrador/Modificar.tsx
+++ src/pages/Administrador/Modificar.tsx
@@ -1,7 +1,91 @@
+import { useFormularioJefe } from "../../hooks/Administrador/useFormularioJefe";
 import Navegacion from "../Administrador/components/Navegacion";
+import FormularioJefe from "./components/Formulario";
+import { useBusqueda } from "../../hooks/Administrador/useBusqueda";
 
 export default function Modificar() {
+    const { formData, inputValidity, handleInputChange, handleCarreraChange, setFormData, } = useFormularioJefe();
+
+    const { search, currentPage, resultadosFiltrados, totalPaginas, resultadosVisibles, handleChange, handlePageChange, handleSeleccionarUsuario } = useBusqueda(setFormData);
     return (
-        <Navegacion />
+        <>
+            <Navegacion />
+
+            <section className="flex justify-center w-full py-10 max-w-laptop mx-auto gap-10">
+                <div className="flex-2 flex gap-5 flex-col">
+                    <h2 className="text-3xl font-bold">Modificar usuario</h2>
+                    <FormularioJefe
+                        formData={formData}
+                        handleInputChange={handleInputChange}
+                        handleCarreraChange={handleCarreraChange}
+                        inputValidity={inputValidity}
+                    />
+                </div>
+
+                <div className="flex-3 flex flex-col gap-4 w-full relative">
+                    <h2 className="text-3xl font-bold">Buscar usuario</h2>
+                    <p className=" py-0.5">
+                        Buscar usuario por su clave o nombre y seleccionarlo para modificar
+                    </p>
+
+                    <form className="w-full">
+                        <label htmlFor="default-search" className="sr-only">Buscar</label>
+                        <div className="relative">
+                            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <svg className="w-4 h-4 text-blue-700 dark:text-blue-700" aria-hidden="true" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                </svg>
+                            </div>
+                            <input
+                                type="search"
+                                id="default-search"
+                                className="block w-full p-4 ps-10 text-sm border rounded-lg bg-white border-gray-600 placeholder-gray-400 text-secondary font-bold focus:ring-blue-500 focus:border-blue-500"
+                                placeholder="ITSH_0000, Luis Enrique ..."
+                                value={search}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </form>
+                    {search.trim() !== "" ? (
+                        resultadosFiltrados.length > 0 ? (
+                            <div className="z-10 w-full mt-2 bg-white border border-gray-300 rounded-lg shadow-lg">
+                                <ul>
+                                    {resultadosVisibles.map((usuario, index) => (
+                                        <li
+                                            key={index}
+                                            className="px-4 py-2 cursor-pointer hover:bg-blue-700 hover:text-white text-sm"
+                                            onClick={() => handleSeleccionarUsuario(usuario)}
+                                        >
+                                            <strong>{usuario.clave}</strong> - {usuario.nombre} {usuario.apellidos}
+                                        </li>
+                                    ))}
+                                </ul>
+
+                                {/* Paginación */}
+                                {totalPaginas > 0 && resultadosFiltrados.length > 9 && (
+                                    <div className="flex justify-center items-center gap-2 p-2 border-t border-gray-200 text-sm bg-gray-50 flex-wrap">
+                                        {Array.from({ length: totalPaginas }, (_, i) => i + 1).map(numero => (
+                                            <button
+                                                key={numero}
+                                                className={`px-2 py-1 rounded ${numero === currentPage
+                                                    ? "bg-blue-700 text-white"
+                                                    : "bg-white hover:bg-gray-200 border"
+                                                    }`}
+                                                onClick={() => handlePageChange(numero)}
+                                            >
+                                                {numero}
+                                            </button>
+                                        ))}
+                                    </div>
+                                )}
+                            </div>
+                        ) : (
+                            <p className="text-center text-blue-500 font-extrabold text-sm">No hay resultados para esta búsqueda</p>
+                        )
+                    ) : null
+                    }
+                </div>
+            </section>
+        </>
     );
 }
