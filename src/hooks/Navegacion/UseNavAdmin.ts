28fdb598f41dc43052c1737c1f409818a29da400
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-15 12:34:51 -0600

    feat: actualizar ruta de importación en useNavAdmin para utilizar alias

--- src/hooks/Navegacion/UseNavAdmin.ts
+++ src/hooks/Navegacion/UseNavAdmin.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { useNavAdministrador } from "../../store/NavAdministrador";
+import { useNavAdministrador } from "@/store/NavAdministrador";
 import { useAuth, useUser } from "@clerk/clerk-react";
 
 export function useNavAdmin() {
