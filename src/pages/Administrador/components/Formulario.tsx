079a0da271d3a16b7870b0125ddabb72a623c26c
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-16 22:31:35 -0600

    fix: refactor componente Formulario para mejorar la gestión de datos y la validación de entradas

--- src/pages/Administrador/components/Formulario.tsx
+++ src/pages/Administrador/components/Formulario.tsx
@@ -1,14 +1,14 @@
 import { formulario, items } from "@/assets/ts/Administrador/Registrar";
-import { type UsuarioJefe } from "@/types";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import WarningOutlinedIcon from "@mui/icons-material/WarningOutlined";
 import Select from "@/pages/Administrador/components/Select";
 import { type FormularioJefeProps } from "@/types";
+import { useFormData } from "@/store/dataJefeCarrera";
 
 
-export default function Formulario({ formData, handleInputChange, handleCarreraChange, inputValidity }: FormularioJefeProps) {
+export default function Formulario({ handleInputChange, handleCarreraChange, inputValidity }: FormularioJefeProps) {
 
-
+    const { formData } = useFormData();
 
 
     return (
@@ -20,32 +20,43 @@ export default function Formulario({ formData, handleInputChange, handleCarreraC
                     </span>
 
                     <div className="relative w-full">
-                        <input
-                            type={type}
-                            name={id}
-                            value={formData[id as keyof UsuarioJefe] as string}
-                            required={required}
-                            pattern={pattern}
-                            placeholder={label}
-                            onChange={handleInputChange}
-                            className={`border font-bold rounded-md p-4 w-full input_hover text-sm pr-10
-                                            ${inputValidity[id] === true
-                                    ? "border-green-500 outline-green-500"
-                                    : ""
-                                }
-                                            ${inputValidity[id] === false
-                                    ? "border-red-500 outline-red-500"
-                                    : ""
-                                }
+                        {(() => {
+                            const validityKey = id === "emailAddresses" ? "correo_usuario" : id;
+                            const isValid = inputValidity[validityKey];
+
+                            return (
+                                <>
+                                    <input
+                                        type={type}
+                                        name={id}
+                                        value={
+                                            id === "emailAddresses"
+                                                ? formData.emailAddresses[0]?.emailAddress || ""
+                                                : id === "clave_empleado"
+                                                    ? formData.publicMetadata.clave_empleado || ""
+                                                    : (formData[id as keyof typeof formData] as string) || ""
+                                        }
+
+                                        required={required}
+                                        pattern={pattern}
+                                        placeholder={label}
+                                        onChange={handleInputChange}
+                                        className={`border font-bold rounded-md p-4 w-full input_hover text-sm pr-10
+                                        ${isValid === true ? "border-green-500 outline-green-500" : ""}
+                                        ${isValid === false ? "border-red-500 outline-red-500" : ""}
                                         `}
-                        />
-                        {inputValidity[id] === true && (
-                            <CheckCircleIcon className="absolute right-3 top-1/2 -translate-y-1/2 text-green-500" />
-                        )}
-                        {inputValidity[id] === false && (
-                            <WarningOutlinedIcon className="absolute right-3 top-1/2 -translate-y-1/2 text-red-500" />
-                        )}
+                                    />
+                                    {isValid === true && (
+                                        <CheckCircleIcon className="absolute right-3 top-1/2 -translate-y-1/2 text-green-500" />
+                                    )}
+                                    {isValid === false && (
+                                        <WarningOutlinedIcon className="absolute right-3 top-1/2 -translate-y-1/2 text-red-500" />
+                                    )}
+                                </>
+                            );
+                        })()}
                     </div>
+
                 </label>
             ))}
 
@@ -66,7 +77,7 @@ export default function Formulario({ formData, handleInputChange, handleCarreraC
                 <span className="text-white px-4 py-2 rounded-lg min-w-2/5 w-fit text-xs bg-secondary font-bold">
                     Carrera que dirigirá
                 </span>
-                <Select items={items} handleChange={handleCarreraChange} carreravalue={formData.carrera} />
+                <Select items={items} handleChange={handleCarreraChange} carreravalue={formData.publicMetadata.carrera ?? ""} />
             </label>
         </form>
     )
